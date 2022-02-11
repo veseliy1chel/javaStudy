6041c24f46eab5b12cf7124852ab00d5c422e7e9
VESELIY1CHEL <voiteshenko2002@gmail.com>
2022-02-11 16:11:25 +0200

    Add member class
    Cat, Human and Robot will take part in future activities

---
+++ Lection2/src/members/Human.java
@@ -0,0 +1,68 @@
+package members;
+
+public class Human implements Runnable,Jumpable {
+    private String name;
+    private int maxJumpLimit;
+    private int maxRunLimit;
+    private boolean canTakePart;
+
+    public Human(String name, int maxJumpLimit, int maxRunLimit) {
+        this.name = name;
+        this.maxJumpLimit = maxJumpLimit;
+        this.maxRunLimit = maxRunLimit;
+        this.canTakePart=true;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getMaxJumpLimit() {
+        return maxJumpLimit;
+    }
+
+    public void setMaxJumpLimit(int maxJumpLimit) {
+        this.maxJumpLimit = maxJumpLimit;
+    }
+
+    public int getMaxRunLimit() {
+        return maxRunLimit;
+    }
+
+    public void setMaxRunLimit(int maxRunLimit) {
+        this.maxRunLimit = maxRunLimit;
+    }
+
+    public boolean isCanTakePart() {
+        return canTakePart;
+    }
+
+    public void setCanTakePart(boolean canTakePart) {
+        this.canTakePart = canTakePart;
+    }
+
+    @Override
+    public void jump(int hight) {
+        if(hight<maxJumpLimit)
+        System.out.println("Human "+name+" overcome this barrier");
+        else{
+            System.out.println("Human "+name+" can`t jump this barrier");
+            canTakePart=false;
+        }
+    }
+
+    @Override
+    public void run(int distance) {
+        if(distance<maxRunLimit){
+            System.out.println("Human "+name+" overcome this running");
+        }
+        else{
+            System.out.println("Human "+name+" can`t overcome this barrier");
+            canTakePart=false;
+        }
+    }
+}
