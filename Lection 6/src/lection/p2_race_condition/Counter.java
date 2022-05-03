dbb4d0d52b7d0db9cd1aceada59bcaac201ccec4
VESELIY1CHEL <voiteshenko2002@gmail.com>
2022-05-03 17:04:42 +0300

    pt2(first part)

---
+++ Lection 6/src/lection/p2_race_condition/Counter.java
@@ -0,0 +1,10 @@
+package lection.p2_race_condition;
+
+public class Counter {
+    private int c;
+
+    public int value(){return c;}
+    public Counter(){c = 0;}
+    public void inc(){c++; }
+    public void decr(){c--;}
+}
